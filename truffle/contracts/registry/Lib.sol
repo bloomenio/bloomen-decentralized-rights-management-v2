a14cec2034b5cd05eb38210bc49c393a511803cc
Nikos Kapsoulis <nkapsoulis@gmail.com>
2019-11-28 16:49:22 +0200

    overlapped dates detection

--- truffle/contracts/registry/Lib.sol
+++ truffle/contracts/registry/Lib.sol
@@ -18,13 +18,15 @@ contract Lib is Users {
 
   struct Claim {
     uint256 creationDate;
-    uint256 claimId; // 2^48 ~ 100 trillion claims
+    uint256 claimId;        // 2^48 ~ 100 trillion claims
     NameValue[] claimData;
     bool claimType;
     uint256 memberOwner;    // 2^24 ~ 16 million members
     bool status;
     uint256 lastChange;
-//    string[] log;
+//    uint48[] log;
+//    uint16 log2;
+//    string log3;
   }
 
   //  uint256 constant private PAGE_SIZE = 10;
@@ -46,29 +48,25 @@ contract Lib is Users {
 //    rightHolderRoles: string or not
     RLPReader.RLPItem memory item = _claimData.toRlpItem();
     RLPReader.RLPItem[] memory itemList = item.toList();  // ((name0, value0), (name, value), (name, value), ...)
-    uint16 split = uint16(itemList[5].toList()[1].toUint());
-    // Split .toUint() Translation
-    if (split > 12591) {
-      //      uint16 dec = (split-48)/256-48;
-      //      split = ((split-48)/256-48)*10+split-12544-(((split-48)/256-48)-1)*256-48;
-      split = split -246*((split-48)/256-48)-12336;
-    } else if (split < 58) {
-      split-=48;
-    } else if (split == 12336) {
-      split = 100;
-    }
+    uint16 split = uint16(bytesToUint(itemList[5].toList()[1].toBytes()));
+    uint48 startDate = uint48(bytesToUint(itemList[2].toList()[1].toBytes()));
+    uint48 endDate = uint48(bytesToUint(itemList[3].toList()[1].toBytes()));
 
+//    claims_[_claimId].log.push(uint48(bytesToUint(string(itemList[2].toList()[1].toBytes()))));
+//    claims_[_claimId].log.push(uint48(bytesToUint(string(itemList[3].toList()[1].toBytes()))));
     maxSplits_[_claimId]=split;
     bool prevStatus = claims_[_claimId].status;
     bool tempMaxSplitOnce = true;
     for(uint256 i = 1; i <= claimIdCounter_; i++) {
       if (_claimType == claims_[i].claimType && _claimId != i) {
-        if(keccak256(abi.encodePacked(itemList[0].toList()[1].toBytes())) == keccak256(abi.encodePacked(claims_[i].claimData[0].value)) // same ISRC/ISWC
-        && keccak256(abi.encodePacked(itemList[1].toList()[1].toBytes())) == keccak256(abi.encodePacked(claims_[i].claimData[1].value)) // same countries
-        && keccak256(abi.encodePacked(itemList[2].toList()[1].toBytes())) == keccak256(abi.encodePacked(claims_[i].claimData[2].value)) // must check end1 >= start2
-        && keccak256(abi.encodePacked(itemList[3].toList()[1].toBytes())) == keccak256(abi.encodePacked(claims_[i].claimData[3].value)) // must check start1 <= end2
-        && keccak256(abi.encodePacked(itemList[4].toList()[1].toBytes())) == keccak256(abi.encodePacked(claims_[i].claimData[4].value)) // same useTypes/rightTypes
+        if(keccak256(itemList[0].toList()[1].toBytes()) == keccak256(claims_[i].claimData[0].value) // same ISRC/ISWC
+        && keccak256(itemList[1].toList()[1].toBytes()) == keccak256(claims_[i].claimData[1].value) // same countries
+        && endDate >= uint48(bytesToUint(bytes(claims_[i].claimData[2].value))) // must check end1 >= start2
+        && startDate <= uint48(bytesToUint(bytes(claims_[i].claimData[3].value))) // must check start1 <= end2
+        && keccak256(itemList[4].toList()[1].toBytes()) == keccak256(claims_[i].claimData[4].value) // same useTypes/rightTypes
         ){
+//          claims_[i].log.push(uint48(bytesToUint(bytes(claims_[i].claimData[2].value))));
+//          claims_[i].log.push(uint48(bytesToUint(bytes(claims_[i].claimData[3].value))));
           if (newClaim) {
             maxSplits_[i]+=split;                // build maxSplits_[i]
             if (maxSplits_[i]>100) {
@@ -101,6 +99,31 @@ contract Lib is Users {
             tempMaxSplitOnce=false;
           }
         }
+      }
+    }
+  }
+
+//  function translateRLPUint(uint16 _number) private pure returns (uint16) {
+//
+//    if (_number > 12591) {
+//      //      uint16 dec = (split-48)/256-48;
+//      //      split = ((split-48)/256-48)*10+split-12544-(((split-48)/256-48)-1)*256-48;
+//      return _number -246*((_number-48)/256-48)-12336;
+//    } else if (_number < 58) {
+//      return _number-48;
+//    } else { // if (_number == 12336)
+//      return 100;
+//    }
+//  }
+
+  function bytesToUint(bytes s) private constant returns (uint result) {
+    bytes memory b = s;
+    uint i;
+    result = 0;
+    for (i = 0; i < b.length; i++) {
+      uint c = uint(b[i]);
+      if (c >= 48 && c <= 57) {
+        result = result * 10 + (c - 48);
       }
     }
   }
